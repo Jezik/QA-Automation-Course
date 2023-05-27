7470e322a9ae78d73d41eedef25c5e181c422bfd
Dmitriy Baev <dmitriy.baevtech@gmail.com>
2023-05-27 15:30:33 +0200

    Declaration of test functions moved to login_page.py and shop_page.py. From within _test.py we just call them now. Add .vscode/settings.json in order VSCode can determine the paths. Small refactoring

--- tests/login_page_test.py
+++ tests/login_page_test.py
@@ -1,4 +1,3 @@
-import pytest
 import time
 import sys
 sys.path.append("./pages")
@@ -11,15 +10,16 @@ from helpers import *
 from urls import *
 
 
-browser = webdriver.Chrome()
-browser.get(login_page)
+driver = webdriver.Chrome()
+driver.get(login_page)
 
-type_text_to_input(browser, "standard_user", USERNAME_XPATH)
-type_text_to_input(browser, "secret_sauce", PASSWORD_XPATH)
-click_on_element(browser, LOGIN_BUTTON_XPATH)
+type_text_to_input(driver, "standard_user", USERNAME_XPATH)
+type_text_to_input(driver, "secret_sauce", PASSWORD_XPATH)
+click_on_element(driver, LOGIN_BUTTON_XPATH)
 
 time.sleep(3)
 
 # If the login was successful we should see the label PRODUCTS_LABEL_XPATH
-def test_if_login_successful():
-    assert check_if_element_exists(browser, PRODUCTS_LABEL_XPATH) == True
+test_if_login_successful(driver)
+
+print("Test finished.")
--- tests/shop_page_test.py
+++ tests/shop_page_test.py
@@ -1,4 +1,3 @@
-import pytest
 import time
 import sys
 sys.path.append("./pages")
@@ -12,17 +11,17 @@ from helpers import *
 from urls import *
 
 
-browser = webdriver.Chrome()
-browser.get(login_page)
+driver = webdriver.Chrome()
+driver.get(login_page)
 
-type_text_to_input(browser, "standard_user", USERNAME_XPATH)
-type_text_to_input(browser, "secret_sauce", PASSWORD_XPATH)
-click_on_element(browser, LOGIN_BUTTON_XPATH)
+type_text_to_input(driver, "standard_user", USERNAME_XPATH)
+type_text_to_input(driver, "secret_sauce", PASSWORD_XPATH)
+click_on_element(driver, LOGIN_BUTTON_XPATH)
 
 time.sleep(3)
 
-click_on_element(browser, ADD_TO_CART_SECOND_ITEM_XPATH)
+click_on_element(driver, ADD_TO_CART_SECOND_ITEM_XPATH)
 
-def test_item_added_to_cart():
-    assert check_if_element_exists(browser, SHOPPING_CART_BADGE_XPATH)
-    assert get_elem_text(browser, SHOPPING_CART_BADGE_XPATH) == "1"
+test_item_added_to_cart(driver)
+
+print("Tests finished.")
